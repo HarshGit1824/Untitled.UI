040ceedb0e0c9bfff51775209d1dec0f4ac7cd1d
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-12-09 14:04:55 +0000

    Layout done

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Home from "./pages/Home";
+
 
 const appEl = document.querySelector("#app");
 const root = createRoot(appEl);
 root.render(
   <React.StrictMode>
-    <Duster />{" "}
+    <Home/>
   </React.StrictMode>,
 );
 
-function Duster() {
-  return (
-    <h1 className="cursor-pointer text-3xl font-bold text-zinc-900 underline hover:text-red-700 md:text-4xl">
-      I am a duster
-    </h1>
-  );
-}
+// function Duster() {
+//   return (
+//     <h1 className="cursor-pointer text-3xl font-bold text-zinc-900 underline hover:text-red-700 md:text-4xl">
+//       I am a duster
+//       <IconBrandGithub />
+//       <IconBrandLinkedin />
+//     </h1>
+//   );
+// }
