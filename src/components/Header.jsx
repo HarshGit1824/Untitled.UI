4836b8a1f02f4b0c87775870ecf336bde7ca56ec
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-12-09 14:21:39 +0000

    Add favicon nd custom font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header className='flex gap-2'>
+      <i>ICON</i>
+      <p>Untitled UI</p>
+    </header>
   )
 }
 
