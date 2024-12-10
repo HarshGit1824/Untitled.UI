cb7817929705fd0ee77b809314be7fc3efc00db9
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-12-10 14:23:14 +0000

    Form component

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
+import {
+  RiFacebookCircleFill,
+  RiChat3Line,
+  RiPhoneLine,
+  RiMap2Line,
+  RiTwitterXFill,
+  RiLinkedinBoxLine,
+} from "react-icons/ri";
 
 const Aside = () => {
-  return (
-    <div>
-      Aside
-    </div>
-  )
-}
+  const contactInfo = [
+    {
+      id: 0,
+      title: "Chat to us",
+      description: "Our friendly team",
+      action: "hi@untitledui.com",
+      icon: RiChat3Line,
+    },
+    {
+      id: 1,
+      title: "Visit us",
+      description: "Come say hello at our office HQ",
+      action: "100 Smith Street Collingwood VIC 3066 AU",
+      icon: RiMap2Line,
+    },
+    {
+      id: 2,
+      title: "Call us",
+      description: "Mon-Fri from 8am to 5pm.",
+      action: "+1 (555) 000-0000",
+      icon: RiPhoneLine,
+    },
+  ];
 
-export default Aside
+  return (
+    <div className="flex min-h-screen flex-col justify-between">
+      {/* Contact Information */}
+      <section>
+        {contactInfo.map((info) => {
+          return (
+            <div className="mb-5" key={info.id}>
+              {<info.icon size={25} />}
+              <h3 className="text-lg font-bold">{info.title}</h3>
+              <p className="mb-2 text-slate-700">{info.description}</p>
+              <p>{info.action}</p>
+            </div>
+          );
+        })}
+      </section>
+
+      {/* Social */}
+      <section className="flex gap-1 text-lime-700">
+        <RiFacebookCircleFill  size={30}/>
+        <RiTwitterXFill size={30}/>
+        <RiLinkedinBoxLine size={30}/>
+      </section>
+    </div>
+  );
+};
+
+export default Aside;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Header from '../components/Header'
-import Aside from '../components/Aside'
-import Form from '../components/Form'
+import React from "react";
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+import Form from "@/components/Form";
 
 const Home = () => {
   return (
-    <div>
-      <Header/>
-      <Aside/>
-      <Form/>
+    <div className="min-h-screen bg-zinc-50 px-4 py-3 md:px-8">
+      <Header />
+      <main className="grid grid-cols-8">
+        <div className="col-span-2 hidden md:block">
+          <Aside />
+        </div>
+        <div className="col-span-full rounded-lg p-5 md:col-span-6 md:bg-lime-400">
+          <Form />
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
